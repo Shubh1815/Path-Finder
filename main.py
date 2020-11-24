a4e3797bebd9af908970ff17af8ebda93c1f4c6e
Shubh1815 <shubhparmar14@gmail.com>
2020-11-24 16:44:32 +0530

    embed pygame win into tkinter

--- main.py
+++ main.py
@@ -1,24 +1,59 @@
+import os
+import threading
+import tkinter as tk
 import pygame
 from grid import Grid
 
-pygame.init()
-win = pygame.display.set_mode((800, 501))
-pygame.display.set_caption("PATH FINDER")
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-RUNNING = True
+        self.minsize(800, 501)
+        self.resizable(0, 0)
+        self.title("Path Finder")
 
-win.fill((20, 39, 78))
+        container = tk.Frame(self, width="800", height="501", bg="black")
+        container.rowconfigure(0, weight=1)    # Expanding the 1st row
+        container.columnconfigure(1, weight=1) # Expanding the 2nd column
+        container.pack(fill=tk.X)
 
-grid = Grid(win, 50, 50)
-grid.initialize()
-grid.generate_maze()
+        embed = tk.Frame(container, width=501, height=501)
+        embed.grid(row=0, column=0, padx=5, pady=5)
 
-while RUNNING:
+        self.toolkit = tk.Frame(container, bg="#30475e")
+        self.toolkit.grid(
+            row=0,
+            column=1,
+            sticky='NSEW',
+            padx=5,
+            pady=5
+        )
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
+        os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+        self.update()
 
-    pygame.display.update()
+        pygame.display.init()
+        win = pygame.display.set_mode((501, 501))
+        win.fill((20, 39, 78))
 
-pygame.quit()
+        grid = Grid(win, 50, 50)
+        grid.initialize()
+        pygame.display.update()
+
+        self.toolkit_section()
+
+
+    def toolkit_section(self):
+
+        label = tk.Label(
+                    self.toolkit,
+                    text="PATH-FINDER",
+                    bg="#30475e",
+                    fg="#ffffff"
+                )
+        label.config(font=("Helvetica", 24))
+        label.pack(fill=tk.X)
+
+if __name__ == '__main__':
+    root = App()
+    root.mainloop()
